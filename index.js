312015f6e931fd9ea589826fbdf3054751dba728
FOREVER <90952986+well300@users.noreply.github.com>
2023-05-16 01:22:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const fs = require('fs/promises');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 const quotesFilePath = path.join(__dirname, 'quotes.json');
 
